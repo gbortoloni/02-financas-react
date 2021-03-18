579f08be6989d404e18973a1edbe97a2301bb3b6
Giuliano Bortoloni <giulianoagria@hotmail.com>
2021-03-18 19:45:44 -0300

    feat(front): criando modal e salvando na api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
+import { NewTrasactionModalOpen } from './components/NewTransactionModal'
 
 import { GlobalStyle } from './styles/global'
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTrasactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTrasactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTrasactionModalOpen
+        isOpen={isNewTrasactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
